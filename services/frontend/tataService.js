a847a2b2cead74435d71ba8c42d573edfa995999
ajitkushwaha123 <ajitkushwahaswe@gmail.com>
2026-08-08 01:45:08 +0530

    update

--- services/frontend/tataService.js
+++ services/frontend/tataService.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 export async function initiateManualCall(destination_number) {
   // If the user provided a hardcoded phone, use that for testing, else use destination_number
-  const phone = "9311507651"; 
+  const phone = "9311507651";
   const { data } = await axios.post("/api/tata/call/click-to-call", {
-    destination_number: phone || destination_number
+    destination_number: destination_number
   });
 
   if (!data.success) {
